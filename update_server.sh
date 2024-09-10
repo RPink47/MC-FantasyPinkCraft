18b672c175033be2a90ea684579116d06495acff
RPink47 <robinorichter47@gmail.com>
2024-09-10 13:55:40 +0200

    changed server update

--- update_server.sh
+++ update_server.sh
@@ -1,21 +1,7 @@
 
 
-find_and_download () {
-    curl -s "$1" > ~/tmp.json
-    cat ~/tmp.json | jq -r '.[] | select(.type=="file") | .download_url' | while read url; do
-        echo "Downloading: $url"
-        curl -L -o $2$(basename $url) $url
-    done
-    cat directory.json | jq -r '.[] | select(.type=="dir") | .url' | while read dir_url; do
-        new_dir= basename $dir_url
-        mkdir "$2$new_dir"
-        find_and_download $dir_url "$2$new_dir"
-    done
-}
-
-
-while getopts "mpco" opt; do
-    case ${opt} in
+while getopts "mpo" opt; do
+    case $opt in
         m)
             curl -s "https://api.github.com/repos/RPink47/MC-FantasyPinkCraft/contents/intalations/mods" > ~/tmp.json
 
@@ -33,12 +19,6 @@ while getopts "mpco" opt; do
                 echo "Downloading: $url"
                 curl -L -o ~/server/plugins/$(basename $url) $url
             done
-            ;;
-        c)
-            
-            find_and_download https://api.github.com/repos/RPink47/MC-FantasyPinkCraft/contents/intalations/config ~/server/config/
-            # Extract and download files
-
             ;;
         o)
             file=$OPTARG
