bdcd9c235b3d71f7e772eac6519abb2b1f86a07b
RPink47 <robinorichter47@gmail.com>
2024-09-10 16:03:08 +0200

    work please

--- update_server.sh
+++ update_server.sh
@@ -31,7 +31,7 @@ while getopts "mpco" opt; do
                 tmp= basename $dir_url
                 name=${tmp%%\?*}
                 curl -s "https://api.github.com/repos/RPink47/MC-FantasyPinkCraft/contents/instalations/config/$name" > ~/dir_tmp.json
-                mkdir ~/server/config/$name
+                mkdir "~/server/config/$name"
                 cat ~/dir_tmp.json | jq -r '.[] | select(.type=="file") | .download_url' | while read url; do
                     curl -L -o ~server/config/$name/$(basename $url) $url
                 done
@@ -39,9 +39,9 @@ while getopts "mpco" opt; do
                     sub_tmp= basename $sub_dir_url
                     sub_name=${sub_tmp%%\?*}
                     curl -s "https://api.github.com/repos/RPink47/MC-FantasyPinkCraft/contents/instalations/config/$sub_name" > ~/sub_dir_tmp.json
-                    mkdir ~server/config/$name/$sub_name
+                    mkdir "~server/config/$name/$sub_name"
                     cat ~/sub_dir_tmp.json | jq -r '.[] | select(.type=="file") | .download_url' | while read url; do
-                        curl -L -o ~server/config/$name/$sub_name/$(basename $url) $url
+                        curl -L -o "~server/config/$name/$sub_name/$(basename $url) $url"
                     done
                 done
             done
